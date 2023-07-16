c42a3308c974c28d54d8375192df09229a334545
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-07-16 01:02:46 -0700

    Update Installation Instructions

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install tools
+brew install java
+brew install node
+brew install firebase-cli
+brew install fastlane
+brew install swiftlint
+
+# Ensure that everything on the system is up-to-date
+brew upgrade
+
+# Non-automated:
+# - Install Xcode and all relevant SDKs/simulators by hand.
+# - Startup the simulator to check that it is working
+# - Install the GitHub Action Runner tools
+# - Setup the GitHub Action Runner tools to connect to GitHub
